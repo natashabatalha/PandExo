a3b864cefabc850d414d24c589e63c54cf4345fd
natashabatalha <neb149@psu.edu>
2017-07-10 13:10:02 -0400

    Bash setup file for conda users

---
+++ pandexo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USRDIR=$HOME
+echo 'USRDIR=$HOME' >>~/.bash_profile
+
+########## BLOCK TO SET STELLAR DATA################
+mkdir $USRDIR/pysynphot_data 
+mv synphot5.tar* $USRDIR/pysynphot_data 
+tar -xvf $USRDIR/pysynphot_data/synphot5.tar || tar -xvf $USRDIR/pysynphot_data/synphot5.tar.gz
+mv grp/hst/cdbs/grid $USRDIR/pysynphot_data
+rm -r grp
+echo 'export PYSYN_CDBS="$USRDIR/pysynphot_data"' >>~/.bash_profile
+
+############ BLOCK TO SET PANDEIA REFERENCE DATA #########################
+
+tar -xvf pandeia_data-1.0.tar.gz || tar -xvf pandeia_data-1.0.tar
+echo 'export pandeia_refdata="$USRDIR/pandeia_data-1.0"' >>~/.bash_profile
+
+##############Make sure your bash profile is sourced
+source ~/.bash_profile
+
+######### Install PandExo Engine, which will also install Pandeia##############
+pip install pandexo.engine 
+
+########### Configure Conda to find STScI packages ###########
+conda config --add channels http://ssb.stsci.edu/astroconda
+
+########### Instal conda/stsci specific packages
+conda install pyfftw
+
+conda install numpy scipy astropy pyfftw pysynphot photutils
+
+########## RUN TEST ###############
+python run_test.py
---
+++ run_test.py
@@ -0,0 +1,25 @@
+import warnings
+warnings.filterwarnings('ignore')
+import pandexo.engine.justdoit as jdi # THIS IS THE HOLY GRAIL OF PANDEXO
+import numpy as np
+import os
+exo_dict = jdi.load_exo_dict()
+exo_dict['observation']['sat_level'] = 80    #saturation level in percent of full well 
+exo_dict['observation']['noccultations'] = 2 #number of transits 
+exo_dict['observation']['R'] = None          #fixed binning. I usually suggest ZERO binning.. you can always bin later 
+                                             #without having to redo the calcualtion
+exo_dict['observation']['fraction'] = 1.0    #fraction of time in transit versus out = in/out
+exo_dict['observation']['noise_floor'] = 0   #this can be a fixed level or it can be a filepath 
+exo_dict['star']['type'] = 'phoenix'        #phoenix or user (if you have your own)
+exo_dict['star']['mag'] = 8.0               #magnitude of the system
+exo_dict['star']['ref_wave'] = 1.25         #For J mag = 1.25, H = 1.6, K =2.22.. etc (all in micron)
+exo_dict['star']['temp'] = 5500             #in K 
+exo_dict['star']['metal'] = 0.0             # as log Fe/H
+exo_dict['star']['logg'] = 4.0  
+exo_dict['planet']['type'] = 'constant'
+exo_dict['planet']['depth'] = 0.01                      #other options include "um","nm" ,"Angs", "secs" (for phase curves)
+exo_dict['planet']['transit_duration'] = 2.0*60.0*60.0 
+
+print('Starting TEST run')
+jdi.run_pandexo(exo_dict, ['NIRSpec G140H'], save_file=False)
+print('SUCCESS') 
