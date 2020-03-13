02c8398d7c9d2742df4e3b7da5c00b1e7813727a
Natasha Batalha <natasha.e.batalha@gmail.com>
2020-03-12 18:04:39 -0700

    docs for r definition

--- pandexo/engine/bintools.py
+++ pandexo/engine/bintools.py
@@ -26,7 +26,8 @@ def binning(x, y,  dy=None, binwidth=None, r=None,newx= None, log = False, nan=F
 	binwidth : float 
 		(Optional) constant bin width in same units as x 
 	r : float 
-		(Optional) constant resolution to bin to 
+		(Optional) constant resolution to bin to. R is defined as w[1]/(w[2] - w[0]) 
+		to maintain consistency with `pandeia.engine`
 	newx : array, float 
 		(Optional) new x axis to bin to 
 	log : bool
