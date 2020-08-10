63c4c1e49e534bc685f235e72db7bda3eb7b4f4a
nespinoza <nespinoza@stsci.edu>
2020-08-09 23:21:32 -0400

    Solved resolver update bug so planet name stays in place.

--- pandexo/engine/run_online.py
+++ pandexo/engine/run_online.py
@@ -294,6 +294,7 @@ class CalculationNewHandler(BaseHandler):
             try:
                 planet_name = self.get_argument("planetname")
                 planet_data = get_target_data(planet_name)[0]
+                exodata['planet']['planetname'] = planet_data['canonical_name']
                 # for item in planet_data:
                 #     print("{}: {}".format(item, planet_data[item]))
                 # star
